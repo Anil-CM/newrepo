ef2e5e0bded630c40563aaedf3978d81016a1f68
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2025-11-06 09:14:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
 
 
-# resource "ibm_is_vpc" "vpc" {
-#   name = var.vpc_name
-#   tags = var.vpc_tags
-# }
-# This resource will destroy (potentially immediately) after null_resource.next
+
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_900_seconds" {
+resource "time_sleep" "wait_in_seconds" {
   depends_on = [null_resource.previous]
 
   create_duration = "10s"
@@ -15,9 +11,5 @@ resource "time_sleep" "wait_900_seconds" {
 
 # This resource will create (at least) 900 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_900_seconds]
+  depends_on = [time_sleep.wait_in_seconds]
 }
-
-#data "ibm_is_vpc" "example" {
-#  name = var.vpc_name
-#}
