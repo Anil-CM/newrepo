020c57ee0557af8f26cb5dfac559c05caed229a0
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2026-04-06 19:45:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-
-
-
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_in_seconds" {
-  depends_on = [null_resource.previous]
-
-  create_duration = "300s"
-}
-
-# This resource will create (at least) 900 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_in_seconds]
+output "print_message" {
+  value = var.message
 }
