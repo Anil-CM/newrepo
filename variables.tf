4322e80d1c7a7a8615737e4f73f0589473646278
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2026-04-06 19:45:52 +0530

    Add message variable with description

--- variables.tf
+++ variables.tf
@@ -1,14 +1,4 @@
-#variable "vpc_name"{
-#  type = string
-#  default="testvpcone"
-#}
-
-#variable "vpc_tags" {
-#  type = list(string)
-#  default = ["tag:test1", "tag:test2"]
-#}
-
-#variable "api_key" {
-#  type = string
-#  default = ""
-#}
+variable "message" {
+  description = "Message to display"
+  type        = string
+}
