d21176c9798ea688ae6ad13c14d622cfd4d3c7ca
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2026-04-14 15:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,44 @@
-output "print_message" {
-  value = var.message
+#output "print_message" {
+#  value = var.message
+#}
+
+variable "input_config" {
+  description = "Complex input object"
+  type = object({
+    name  = string
+    value = object({
+      name     = string
+      schedule = object({
+        unit      = string
+        frequency = number
+      })
+      retention = object({
+        duration = number
+        unit     = string
+      })
+      use_default_backup_target = bool
+    })
+    metadata = object({
+      type = string
+    })
+  })
+}
+
+# Print full object
+output "full_input" {
+  value = var.input_config
+}
+
+# Pretty JSON output
+output "full_input_json" {
+  value = jsonencode(var.input_config)
+}
+
+# Example: access nested fields
+output "policy_name" {
+  value = var.input_config.value.name
+}
+
+output "backup_frequency" {
+  value = var.input_config.value.schedule.frequency
 }
